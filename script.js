bdeea0a98296c2b9c829024e0d5df4dd94b09f41
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-04-07 20:48:13 +0200

    compteur decomtpe alarme ok

--- script.js
+++ script.js
@@ -1,5 +1,114 @@
 
 
+// -----------------------------------horloge
+function time(){
+    date = new Date();
+    // console.log(date)
+    heure = date.getHours();
+    minutes = date.getMinutes();
+    secondes = date.getSeconds();
+    
+    if(secondes<10)
+    {
+        secondes = ""+0+secondes
+    }
+    if(minutes<10)
+    {
+       minutes = ""+0+minutes
+    }
+    
+    // horloge = `${heure} : ${minutes} : ${secondes}`
+    horloge = heure + " : " + minutes + " : " + secondes
+    // console.log(horloge)
+    
+    document.getElementById("heure").innerText = horloge 
+
+    // -------recupère les horloges ajoutée
+    var parentDOM = document.getElementById("alarmes_crees");
+    var alarme_utilisateur=parentDOM.getElementsByClassName("div1");
+    var message_utilisateur=parentDOM.getElementsByClassName("message_cache");
+
+    // console.log(alarme_utilisateur.length);
+ 
+    // ----boucle pour comparer la valeur de l'horloge aux alarmes
+    for(   i = 0; i<alarme_utilisateur.length; i++ )
+    {
+        if(horloge == alarme_utilisateur[i].innerHTML)
+        {
+            alarme_utilisateur[i].classList.add("alarme_check","animate__animated", "animate__flash");
+            message_utilisateur[i].classList.remove("message_cache");
+            message_utilisateur[i].classList.add("message_apparait");
+        }
+
+        // recuperation des heure/minute/seconde pour comparer
+        alarme_utilisateur_formatee = alarme_utilisateur[i].innerHTML
+        temps = alarme_utilisateur_formatee.split(' : ')
+        temps_reel = horloge.split(' : ');
+
+        heure_restantes =  temps[0] - temps_reel[0];
+        minutes_restantes =  temps[1] - temps_reel[1];
+
+        if(temps[1]<temps_reel[1])
+        {
+            var minutes = parseInt( temps[1],10);
+            minutes = minutes + 60
+       
+            minutes_restantes =  minutes - temps_reel[1];
+        }
+        else{
+
+            minutes_restantes =  temps[1] - temps_reel[1];
+        }
+
+        if(temps[2]<temps_reel[2])
+        {
+            seconde = temps[2]
+            var seconde_2 = parseInt(seconde,10);
+            seconde_2 = seconde_2 + 60
+            seconde_restantes =  seconde_2 - temps_reel[2];
+            minutes_restantes = minutes_restantes -1
+        }
+        else{
+
+            seconde_restantes =  temps[2] - temps_reel[2];
+        }
+        
+        temps_restant_avant_alarme = heure_restantes + " : " + minutes_restantes + " : " + seconde_restantes
+        console.log(temps_restant_avant_alarme)
+        var parent = document.getElementById('alarmes_crees')
+        var div = document.createElement('div');
+        div.setAttribute('id',"temps_restant")
+      
+        // --------------------ajout du temps restant
+        test = document.getElementById("temps_restant")
+        if(test == null)
+        {
+            div.innerHTML = "temps restant avant alarme " + temps_restant_avant_alarme ;
+            parent.appendChild(div)
+            test = document.getElementById("temps_restant").innerHTML 
+            console.log(test)
+        }
+        else{ 
+            $("#temps_restant").remove();
+            div.innerHTML = "temps restant avant alarme " + temps_restant_avant_alarme ;
+            parent.appendChild(div)
+            test = document.getElementById("temps_restant").innerHTML 
+            console.log(test)
+        }
+
+
+    }
+    
+
+    
+
+}
+
+// lance une première fois la fonction pour ne pas avoir de page blanche pendant 1s
+time();
+
+// continue la fonction avec 1s d'interval
+setInterval( time, 1000)
 
 
 
@@ -162,7 +271,7 @@ boutton_down_secondes.addEventListener('click', function(){
 // -------------------------------------------------------boutton ajout alarme
 var creer_alarme = document.getElementById("ajouter_alarme");
 
-creer_alarme.addEventListener('click', function(){
+creer_alarme.addEventListener('click', function(temps_restant_avant_alarme){
     console.log('bouttton clicke')
     var heure = document.getElementById("heure_origine").innerHTML;
     var minutes = document.getElementById("minute_origine").innerHTML;
@@ -184,96 +293,5 @@ creer_alarme.addEventListener('click', function(){
 
 
 
-// -----------------------------------horloge
-function time(){
-    date = new Date();
-    // console.log(date)
-    heure = date.getHours();
-    minutes = date.getMinutes();
-    secondes = date.getSeconds();
-    
-    if(secondes<10)
-    {
-        secondes = ""+0+secondes
-    }
-    if(minutes<10)
-    {
-       minutes = ""+0+minutes
-    }
-    
-    // horloge = `${heure} : ${minutes} : ${secondes}`
-    horloge = heure + " : " + minutes + " : " + secondes
-    // console.log(horloge)
-    
-    
-
-    // -------recupère les horloges ajoutée
-    var parentDOM = document.getElementById("alarmes_crees");
-    var alarme_utilisateur=parentDOM.getElementsByClassName("div1");
-    var message_utilisateur=parentDOM.getElementsByClassName("message_cache");
-
-    // console.log(alarme_utilisateur.length);
- 
-    // ----boucle pour comparer la valeur de l'horloge aux alarmes
-    for(   i = 0; i<alarme_utilisateur.length; i++ )
-    {
-        if(horloge == alarme_utilisateur[i].innerHTML)
-        {
-            alarme_utilisateur[i].classList.add("alarme_check","animate__animated", "animate__flash");
-            message_utilisateur[i].classList.remove("message_cache");
-            message_utilisateur[i].classList.add("message_apparait");
-        }
-
-        // recuperation des heure/minute/seconde pour comparer
-        alarme_utilisateur_formatee = alarme_utilisateur[i].innerHTML
-        temps = alarme_utilisateur_formatee.split(' : ')
-        temps_reel = horloge.split(' : ');
-
-        heure_restantes =  temps[0] - temps_reel[0];
-        minutes_restantes =  temps[1] - temps_reel[1];
-
-        if(temps[1]<temps_reel[1])
-        {
-            var minutes = parseInt( temps[1],10);
-            minutes = minutes + 60
-       
-            minutes_restantes =  minutes - temps_reel[1];
-        }
-        else{
-
-            minutes_restantes =  temps[1] - temps_reel[1];
-        }
-
-        if(temps[2]<temps_reel[2])
-        {
-            seconde = temps[2]
-            var seconde_2 = parseInt(seconde,10);
-            seconde_2 = seconde_2 + 60
-            seconde_restantes =  seconde_2 - temps_reel[2];
-            minutes_restantes = minutes_restantes -1
-
-        }
-        else{
-
-            seconde_restantes =  temps[2] - temps_reel[2];
-        }
-        
-        temps_restant_avant_alarme = heure_restantes + " : " + minutes_restantes + " : " + seconde_restantes
-        console.log(temps_restant_avant_alarme)
-
-
-
-    }
-    
-
-    document.getElementById("heure").innerText = horloge 
-
-}
-
-// lance une première fois la fonction pour ne pas avoir de page blanche pendant 1s
-time();
-
-// continue la fonction avec 1s d'interval
-setInterval( time, 1000)
 
 
