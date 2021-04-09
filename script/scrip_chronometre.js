b2f8bced98cc3ba3b910eddf128858c3bd937d0a
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-04-09 17:49:29 +0200

    chrono ok

--- script/scrip_chronometre.js
+++ script/scrip_chronometre.js
@@ -1,14 +1,7 @@
 
-
-var temps_au_moment_click
-var temps_minuteur_voulu
-var temps_minuteur_voulu
-var temps_minuteur_voulu_parse
-var temps_au_moment_click_parse
 var demarrer_pause = 0
-
 var repere_chronometre
-// -------------------------------------------------------boutton ajout alarme
+// -------------------------------------------------------boutton lancement chrono
 
 lancer_chrono = document.getElementById("demarrer_pause_chrono");
 
@@ -33,6 +26,51 @@ lancer_chrono.addEventListener('click', function(){
 })
 
 
+// ----------------------------------------------------------------------temps intermediaire
+temps_intermediare = document.getElementById("temps_intermediaire");
+
+temps_intermediare.addEventListener('click',function(){
+
+    heure_temps_inter = document.getElementById('heure_origine').innerHTML
+    minutes_temps_inter = document.getElementById('minute_origine').innerHTML
+    seconde_temps_inter = document.getElementById('seconde_origine').innerHTML
+    console.log(heure_temps_inter)
+
+    temps_total_inter = heure_temps_inter + " : " +minutes_temps_inter + " : " + seconde_temps_inter;
+    console.log(temps_total_inter)
+    
+    i=0
+    while(document.getElementById(""+i+"") != null)
+    {
+        i++
+    }
+
+    ajout_temps_inter = document.createElement("div");
+    ajout_temps_inter.setAttribute("id", ""+i+"") 
+    ajout_temps_inter.innerHTML = temps_total_inter
+    parent = document.getElementById("conteneur_temps_intermediare")
+    parent.insertBefore(ajout_temps_inter, parent.children[0])
+
+})
+
+// -------------------------------------------------------------------------------reset
+reset = document.getElementById("reset_chrono");
+
+reset.addEventListener('click',function(){
+
+    document.getElementById('heure_origine').innerHTML = ""
+    document.getElementById('minute_origine').innerHTML = ""
+    document.getElementById('seconde_origine').innerHTML = ""
+    document.getElementById('heure_origine').innerHTML = 0
+    document.getElementById('minute_origine').innerHTML = 0
+    document.getElementById('seconde_origine').innerHTML = 0
+    console.log(heure_temps_inter)
+
+
+})
+
+
+// ----------------------------------------------------------------fonction chronometre
 function chronometre(){
 
     if(Number.isInteger(repere_chronometre))
@@ -88,138 +126,5 @@ setInterval(chronometre,1000)
 
 
 
-
-
-
-
-// -----------------------------------ajout + 1 à l'heure
-var boutton_up_heure = document.getElementById("ajouter_heure");
-
-boutton_up_heure.addEventListener('click', function(){
-
-    var heure_origine = document.getElementById("heure_origine").innerHTML;
-    parseInt(heure_origine, 10);
-    console.log(heure_origine)
-    if(heure_origine==23)
-    {
-        heure_plus_une = 0;
-    }
-    else
-    {
-        var heure_plus_une = ++heure_origine;
-    }
-    document.getElementById("heure_origine").innerText = heure_plus_une 
-
-})
-
-// -----------------------------------enlève 1 à l'heure
-var boutton_down_heure = document.getElementById("enlever_heure");
-
-boutton_down_heure.addEventListener('click', function(){
-
-    var heure_origine = document.getElementById("heure_origine").innerHTML;
-    parseInt(heure_origine, 10);
-    console.log(heure_origine)
-    if(heure_origine==0)
-    {
-        heure_moins_une = 23;
-    }
-    else
-    {
-        var heure_moins_une = --heure_origine;
-    }
-    document.getElementById("heure_origine").innerText = heure_moins_une 
-
-})
-
-
-// -----------------------------------ajout + 1 aux minutes
-var boutton_up_minutes = document.getElementById("ajouter_minute");
-
-boutton_up_minutes.addEventListener('click', function(){
-
-    var minute_origine = document.getElementById("minute_origine").innerHTML;
-    parseInt(minute_origine, 10);
-
-    if(minute_origine==59)
-    {
-        minutes_plus_une = 0;
-    }
-    else
-    {
-        var minutes_plus_une = ++minute_origine;
-    }
-
-    document.getElementById("minute_origine").innerText = minutes_plus_une 
-
-
-})
-
-// -----------------------------------enlève  1 aux minutes
-var boutton_down_minutes = document.getElementById("enlever_minute");
-
-boutton_down_minutes.addEventListener('click', function(){
-
-    var minute_origine = document.getElementById("minute_origine").innerHTML;
-    parseInt(minute_origine, 10);
-    if(minute_origine==0)
-    {
-        minutes_moins_une = 59;
-    }
-    else
-    {
-        var minutes_moins_une = --minute_origine;
-    }
-
-    document.getElementById("minute_origine").innerText = minutes_moins_une 
-
-
-})
-
-// -----------------------------------ajout + 1 aux secondes
-var boutton_up_secondes = document.getElementById("ajouter_seconde");
-
-boutton_up_secondes.addEventListener('click', function(){
-
-    var seconde_origine = document.getElementById("seconde_origine").innerHTML;
-    parseInt(seconde_origine, 10);
-
-    if(seconde_origine==59)
-    {
-        secondes_plus_une = 0;
-    }
-    else
-    {
-        var secondes_plus_une = ++seconde_origine;
-    }
-
-    document.getElementById("seconde_origine").innerText = secondes_plus_une 
-
-        
-
-})
-
-// -----------------------------------enlève  1 aux secondes
-var boutton_down_secondes = document.getElementById("enlever_seconde");
-
-boutton_down_secondes.addEventListener('click', function(){
-
-    var seconde_origine = document.getElementById("seconde_origine").innerHTML;
-    parseInt(seconde_origine, 10);
-
-    if(seconde_origine==0)
-    {
-        secondes_moins_une = 59;
-    }
-    else
-    {
-        var secondes_moins_une = --seconde_origine;
-    }
-
-
-    document.getElementById("seconde_origine").innerText = secondes_moins_une 
- 
-
-})
 
 
